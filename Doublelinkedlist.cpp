030a5224520cad4c47eaee3c13f97d6660a78353
Dhika158 <andhikanurjamil7@gmail.com>
2025-05-15 20:08:15 +0700

    List is empty

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -81,4 +81,13 @@ class Doublelinkedlist
 
         current->next = newNode; // step 9d: current.next = newNode
       }
+
+      void hapus()
+      {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+      }
 }
